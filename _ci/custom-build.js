3e55eb2f84fe3129a5098416a8691a24f7f3bbc7
John Haley <johnh@axosoft.com>
2016-06-23 13:21:08 -0700

    Update to handle new NodeGit lifecycle changes

--- _ci/custom-build.js
+++ _ci/custom-build.js
@@ -1,20 +1,30 @@
-var cp = require('child_process');
+const cp = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function(electronVersion, moduleParentPath, modulePath) {
   return new Promise(function(resolve, reject) {
-    cp.exec(
-      `node lifecycleScripts/install.js --electronVersion=${electronVersion}`,
-      {cwd: modulePath, maxBuffer: Number.MAX_VALUE},
-      function(err, stdout, stderr) {
-        console.log(stdout);
-        console.error(stderr);
-        if (err) {
-          reject(err);
-        }
-        else {
-          resolve();
-        }
-      }
+    let npmrc = 'runtime = electron\n';
+    npmrc += 'disturl = https://atom.io/download/atom-shell\n';
+    npmrc += `target = ${electronVersion.replace('v', '')}`;
+
+    fs.writeFileSync(path.join(modulePath, '.npmrc'), npmrc);
+
+    const spawnedNPM = cp.spawn(
+      'npm',
+      ['install'],
+      {cwd: modulePath}
     );
+
+    spawnedNPM.stdout.on('data', (data) => console.log(data.toString().trim()));
+    spawnedNPM.stderr.on('data', (data) => console.error(data.toString().trim()));
+    spawnedNPM.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      }
+      else {
+        reject(code);
+      }
+    });
   });
 };
--- _ci/custom-prepare.js
+++ _ci/custom-prepare.js
@@ -1,22 +1,5 @@
-var cp = require('child_process');
-
-module.exports = function(cwd) {
-  return new Promise(function(resolve, reject) {
-    cp.exec('npm install --ignore-scripts', { cwd: cwd }, function(err, stdout, stderr) {
-      console.log(stdout);
-      console.error(stderr);
-      if (err) {
-        return reject(err);
-      }
-
-      cp.exec('node lifecycleScripts/prepareForBuild.js', { cwd: cwd }, function(err2, stdout2, stderr2) {
-        console.log(stdout2);
-        console.error(stderr2);
-        if (err2) {
-          return reject(err2);
-        }
-        resolve();
-      });
-    });
-  });
+module.exports = function() {
+  // We only need to make a .npmrc in the module path dir and we have that
+  // data in the `build` task so we'll do it there. Thus, nothing to prepare.
+  return Promise.resolve();
 }
