bc10a8917e96db9f20c35c56bf63c1032a6071d9
John Alden <john.alden@gitkraken.com>
2025-11-14 20:05:50 -0700

    remove server and script compilation

--- _ci/custom-prepare.js
+++
@@ -1,5 +0,0 @@
-module.exports = function() {
-  // We only need to make a .npmrc in the module path dir and we have that
-  // data in the `build` task so we'll do it there. Thus, nothing to prepare.
-  return Promise.resolve();
-}
